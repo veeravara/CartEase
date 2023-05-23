9644493f234dc9bbdccfbf904d9bbe3f8ee16892
veeravara <vara.rae2022.gna@gmail.com>
2023-05-24 00:41:23 +0530

    deployed to database

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-// GRABBING ELEMENTS
-const inputFieldElement=document.getElementById("input-field")
-const buttonElement=document.getElementById("add-button")
-buttonElement.addEventListener("click",buttonFunction)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-function buttonFunction(){
-    let inputEnteredValue=inputFieldElement.value
-    console.log(inputEnteredValue)
+const appSettings={
+    databaseURL:"https://realtime-database-5c9c9-default-rtdb.firebaseio.com/"
 }
+
+const application=initializeApp(appSettings)
+const appDatabase=getDatabase(application)
+const grocerylistinDB=ref(appDatabase,"Grocery Items")
+
+
+
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(grocerylistinDB,inputValue) /*first argument takes reference variable , where data has to be pushed , second argument takes what has to be pushed in database*/
+    
+    
+    console.log(inputValue)
+})
